8c7e1ed75c4225e9d834878b9987bba074a5b2bc
Timofey Ivlev <timofejivlev923@gmail.com>
2025-05-25 17:52:59 +0300

    fix

--- Backend_IdeaCheck/IdeaCheack/BusinessIdeaCheck/permissons.py
+++ Backend_IdeaCheck/IdeaCheack/BusinessIdeaCheck/permissons.py
@@ -2,4 +2,4 @@ from rest_framework.permissions import BasePermission
 
 class IsExpertOrInvestor(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and hasattr(request.user , 'userprofile') and request.user.userprofile.role == 'investor' and request.user.userprofile.role == 'expert'
+        return request.user.is_authenticated and hasattr(request.user , 'userprofile') and request.user.userprofile.role == 'investor' or request.user.userprofile.role == 'expert'
