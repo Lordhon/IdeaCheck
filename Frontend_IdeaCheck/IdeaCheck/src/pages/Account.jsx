f899f1860f1a71ba8bcfd0ad5021f3e36f5172de
Timofey Ivlev <timofejivlev923@gmail.com>
2025-05-31 16:14:32 +0300

    a

--- Frontend_IdeaCheck/IdeaCheck/src/pages/Account.jsx
+++ Frontend_IdeaCheck/IdeaCheck/src/pages/Account.jsx
@@ -47,6 +47,18 @@ const Account = () => {
                     <br />
                     <br />
                     <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
            
                     <button onClick={handleLogout} style={styles.logoutButton}>Выйти</button>
             </div>
