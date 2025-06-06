1fe1f2f4c0749d29815dd5bce8f815f2ab302d3f
Timofey Ivlev <timofejivlev923@gmail.com>
2025-06-07 02:01:28 +0300

    a

--- Backend_IdeaCheck/IdeaCheack/User/serializer.py
+++ Backend_IdeaCheck/IdeaCheack/User/serializer.py
@@ -23,6 +23,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 class UserProfileSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username', read_only=True)
     email = serializers.EmailField(source='user.email', read_only=True)
+    status = serializers.CharField(source='user.status', read_only=True)
 
 
     class Meta:
