e73e9bf7c1aa20cf9e99d766be1912497c76d235
Timofey Ivlev <timofejivlev923@gmail.com>
2025-06-07 00:48:04 +0300

    a

--- Backend_IdeaCheck/IdeaCheack/User/views.py
+++ Backend_IdeaCheck/IdeaCheack/User/views.py
@@ -8,6 +8,7 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
 from django.core.mail import send_mail
 from django.conf import settings
+from functools import wraps
 from rest_framework.permissions import AllowAny
 
 from User.tokens import account_activation_token
