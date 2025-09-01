09d00d07107aaebf9dab954dcbaac29a921d44da
Timofey <lordhon@gmail.com>
2025-09-01 11:47:47 +0300

    fix

--- Backend_IdeaCheck/IdeaCheack/User/models.py
+++ Backend_IdeaCheck/IdeaCheack/User/models.py
@@ -20,9 +20,7 @@ class UserProfile(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     role = models.CharField(choices=ROLE_CHOICES , default='entrepreneur')
-    status = models.CharField(choices=STATUS_CHOICES , default='standart')
-    jopa = models.CharField(max_length=255, null=True, blank=True)
-    siski = models.CharField(max_length=255, null=True, blank=True)
+    status = models.CharField(choices=STATUS_CHOICES , default='standart') 
     def __str__(self):
         return f"{self.user}"
 
